eaf646fac36f6e3f1113f393ae9f6ec4cc3fe9bb
RbsilvaLeandro <rbsilva.leandro@gmail.com>
2022-04-18 20:49:08 -0300

    Creating Products Entity and adding First Migration

--- GeekShopping.Product.API/Program.cs
+++ GeekShopping.Product.API/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.Product.API.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<MySqlContext>(options =>
+//{
+//    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnectionString"), new MySqlServerVersion(new Version(6, 2, 4)));
+//});
+
+builder.Services.AddDbContext<MySqlContext>(options =>
+{
+    options.UseMySql(@"Server=localhost;Port=3306;Database=geekShopping_product_api;Uid=root;Pwd=Nigt@c#52489700", new MySqlServerVersion(new Version(6, 2, 4)));
+});
+
+
 
 var app = builder.Build();
 
